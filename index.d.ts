d6004b3a2354a09feea6201bf73dbd62325e0299
Alex West <alex.west@applicable.com>
2016-08-08 11:36:40 +0100

    Modified ts def to work

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
-export module Typer {
-    function detect(value: Object): String;
-    function cast(value: Object, type: String): Object;
+declare module Typer{
+  function detect(value: Object): String;
+  function cast(value: Object, type: String): Object;
+}
+
+declare module "typer"{
+    export default Typer;
 }
